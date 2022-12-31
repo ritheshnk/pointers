cf670ac0c29d392f18c8af0a6a364a47113825b4
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-12-31 15:15:14 +0530

    Add files via upload

---
+++ string_pointer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char mov[]="Rithesh is Dumb";
+    char * mov1="Rithesh is not Dumb";
+    puts(mov1);
+    printf("\nafter changing\n");
+    mov1="Rithesh WIll be Placed on or Before March";
+    puts(mov1);
+    return 0;
+}
